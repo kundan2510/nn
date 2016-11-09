a4596d0165c987da3eeac73f602b19ed76cccf09
Kundan Kumar <kundankumar2510@gmail.com>
2016-11-09 04:22:47 -0500

    varying beta included

--- param_search_cluster.py
+++ param_search_cluster.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import numpy
 
 parser = argparse.ArgumentParser(description='Generating images pixel by pixel')
 
@@ -13,21 +14,22 @@ theano_flag = "THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32,lib.cnmem=0.
 cluster_command = "jobdispatch --gpu --duree=20:00:00 --env={} --project=jvb-000-ag".format(theano_flag)
 cmd = "python -u experiments/vae_pixel_2/mnist.py"
 
-assert(args.vary in ['alpha_iters', 'num_layers', 'filter_sizes', 'latent_dim', 'dim_pix', 'nothing' ])
+assert(args.vary in ['alpha_iters', 'beta', 'num_layers', 'filter_sizes', 'latent_dim', 'dim_pix', 'nothing' ])
 
 alpha_iters = [5000, 6000, 7000]
 num_layers = [12, 14, 16, 18, 20, 24]
 filter_sizes = [5,7]
 latent_dim = [4, 8, 12, 16, 20, 24]
 dim_pix = [4, 8, 12, 16, 20, 24]
+beta = numpy.arange(1, 5, 0.15)
 
-vary_arr_dict = {'alpha_iters': alpha_iters, 'num_layers':num_layers, 'filter_sizes':filter_sizes, 'latent_dim':latent_dim, 'dim_pix':dim_pix}
+vary_arr_dict = {'alpha_iters': alpha_iters, 'num_layers':num_layers, 'filter_sizes':filter_sizes, 'latent_dim':latent_dim, 'dim_pix':dim_pix, 'beta' : beta}
 
-params = {'alpha_iters': 6000, 'num_layers':12, 'filter_sizes':5, 'latent_dim':16, 'dim_pix':16, 'algo': 'cond_z_bias'}
+params = {'alpha_iters': 6000, 'num_layers':12, 'filter_sizes':5, 'latent_dim':16, 'dim_pix':16, 'algo': 'cond_z_bias', 'beta' : 1}
 
 if args.vary == "nothing":
     for i in range(5):
-        FLAGS = "-L {num_layers} -ait {alpha_iters} -fs {filter_sizes} -ldim {latent_dim} -dpx {dim_pix} -algo {algo}".format(**params)
+        FLAGS = "-L {num_layers} -ait {alpha_iters} -fs {filter_sizes} -ldim {latent_dim} -dpx {dim_pix} -algo {algo} -beta {beta}".format(**params)
         final_command = "{} {} {}".format(cluster_command, cmd, FLAGS)
         print final_command
         os.system(final_command)
@@ -39,7 +41,7 @@ vary_arr = vary_arr_dict[args.vary]
 for i,val in enumerate(vary_arr):
     params[args.vary] = val
 
-    FLAGS = "-L {num_layers} -ait {alpha_iters} -fs {filter_sizes} -ldim {latent_dim} -dpx {dim_pix} -algo {algo}".format(**params)
+    FLAGS = "-L {num_layers} -ait {alpha_iters} -fs {filter_sizes} -ldim {latent_dim} -dpx {dim_pix} -algo {algo} -beta {beta}".format(**params)
     final_command = "{} {} {}".format(cluster_command, cmd, FLAGS)
     print final_command
     os.system(final_command)
